5421d7d8831fdc4a0f03d554343b5582019ba11d
Jonah Cole <jonahcole@yahoo.com>
2022-07-25 14:21:33 -0700

    Finish base todo list functionality

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class List(models.Model):
+    item = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+
+    #This is needed for /admin stuff
+    #will print item name and not "List object 1" "List object 2" etc..
+    def __str__(self):
+        return self.item
--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('delete/<list_id>', views.delete, name='delete'),
+    path('mark_complete/<list_id>', views.mark_complete, name='mark_complete'),
+    path('mark_incomplete/<list_id>', views.mark_incomplete, name='mark_incomplete'),
+    path('edit/<list_id>', views.edit, name='edit'),
 ]
